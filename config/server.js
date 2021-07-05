1914852f9e91562f4a4c6ded07e335d2a5a6b128
Kim McCaskill <kimm@4thdownsoftware.com>
2021-07-05 13:55:36 -0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6fe6d42b46841cd9fa16f0cdede714d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e7c1cef-5e4d-41fe-abfd-3b447d33a177'
+};
